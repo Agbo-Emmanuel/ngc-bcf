0c601245ac876e64f6aed84451d7975588de5d3c
Agbo-Emmanuel <agboe4102@gmail.com>
2024-06-13 19:26:30 +0100

    hh

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -19,7 +19,7 @@ const Landing = () => {
           <div className='nbca_card'>
             <div className='nbca_card_left'>
               <div className='nbca_card_left_top'>
-                <h3>NBCA <span>2024</span></h3>
+                <h3>NGC-BCF <span>2024</span></h3>
               </div>
               <p>Join us at the  NGC Business and Career Affair to date! This year's event
                   promises to be bigger and better, bringing together a vibrant community of business
