dee3dfd00f03bb2c0d0bad88c53e30d159372db2
Agbo-Emmanuel <agboe4102@gmail.com>
2024-06-14 01:57:16 +0100

    ggh

--- src/Pages/landingComponents/About.jsx
+++ src/Pages/landingComponents/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
                 <div className='about_text'>
                     <p>The NGC Business and Career Fair is dedicated to establishing a vibrant forum where believers thrive in business and career pursuits, offering valuable insights, strategies, and networking for growth and collaboration.</p>
                     <h3>Vision</h3>
-                    <p>To raise NGC.<br/>To raise Business & Career Affair in every Nations, every Generations and every system.</p>
+                    <p>To raise NGC.<br/>To raise Business & Career fair in every Nations, every Generations and every system.</p>
                     <h3>Mission</h3>
                     <p>Our mission as NGC is to transform lives and communities by touching, teaching and
                     training people to love, to live and to lead like Jesus.</p>
