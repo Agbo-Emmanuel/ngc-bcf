58a1d1495bdf127793a37394e02eff2d03c4f58e
Agbo-Emmanuel <agboe4102@gmail.com>
2024-06-14 11:42:57 +0100

    ghhh

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -43,6 +43,21 @@ const Landing = () => {
           img = {image3}
           />
           <DontMissIt/>
+          <NgcCard 
+          title = '' 
+          subT = '' 
+          text = "The Business And Career Affair is your one-stop shop for success. Job seeker,seasoned pro, or aspiring entrepreneur - this event is for YOU!. Don't wait! Register now and unlock your full potential!"
+          btn = 'Register Now'
+          img = {image3}
+          />
+          <NgcCard 
+          title = '' 
+          subT = '' 
+          text = "Secure your booth/stall at the largest and most anticipated NGC Business and Career Affair yet! This year, we're bigger and better, bringing together business leaders, career professionals, and aspiring entrepreneurs.  "
+          btn = 'Buy Stall'
+          text2 = "Don't miss this unique opportunity to showcase your products, connect with potential clients, and expand your network. Whether you're a startup or an established business, the NGC Business and Career Affair offers unparalleled exposure and growth opportunities."
+          img = {image3}
+          />
       </div>
     
     </>
