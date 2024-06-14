69d152deb4f76ce063823177ea5e82f50c8d0730
Agbo-Emmanuel <agboe4102@gmail.com>
2024-06-14 14:41:46 +0100

    gjng

--- src/Pages/landingComponents/Host.jsx
+++ src/Pages/landingComponents/Host.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../Css/Host.css'
 import Hostcard from './Hostcard'
-import host1 from '../../assets/host1.webp'
+import host1 from '../../assets/host1.jpg'
 
 const Host = () => {
 
@@ -11,19 +11,18 @@ const Host = () => {
     
         <div className='host'>
             <div className='the_host'>
-                <h1>Hosts</h1>
+                <h1>Host</h1>
                 <div className='host_img'>
-                    <Hostcard img = {host1} name = 'Dami Oluwatoyinbo' job = 'KingRaiser, Creative Genius & Pastor of KINGS' />
-                    <Hostcard img = {host1} name = 'Toju Oluwatoyinbo' job = 'Relationship Coach' />
+                    <Hostcard img = {host1} name = 'Pastor Otito Ishola' job = 'Head Pastor, RCCG Next generation Church' />
                 </div>
             </div>
-            <div className='the_host'>
+            {/* <div className='the_host'>
                 <h1>Keynote Speaker</h1>
                 <div className='host_img'>
                     <Hostcard img = {host1} name = 'Dami Oluwatoyinbo' job = 'KingRaiser, Creative Genius & Pastor of KINGS' />
                 </div>
-            </div>
-            <div className='the_host'>
+            </div> */}
+            {/* <div className='the_host'>
                 <h1>Panel Speakers</h1>
                 <div className='host_img'>
                     <Hostcard img = {host1} name = 'Dami Oluwatoyinbo' job = 'KingRaiser, Creative Genius & Pastor of KINGS' />
@@ -32,7 +31,7 @@ const Host = () => {
                     <Hostcard img = {host1} name = 'Dami Oluwatoyinbo' job = 'KingRaiser, Creative Genius & Pastor of KINGS' />
                     <Hostcard img = {host1} name = 'Dami Oluwatoyinbo' job = 'KingRaiser, Creative Genius & Pastor of KINGS' />
                 </div>
-            </div>
+            </div> */}
         </div>
     
     </>
